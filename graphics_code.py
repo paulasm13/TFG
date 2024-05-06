106d87668add155de0f213f1e2d4cb644cd45217
paulasm13 <paula13sepulved@hotmail.com>
2024-05-06 19:54:22 +0200

    Churn Rate graph

--- graphics_code.py
+++ graphics_code.py
@@ -22,6 +22,7 @@ def get_graphs():
     cursor = conn.cursor()
 
     author_graph(cursor)
+    churn_graph(cursor)
 
     print("Graphics created in '.png' files.")
 
@@ -29,6 +30,7 @@ def get_graphs():
     conn.close()
 
 
+# Grafica de lineas por autor
 def author_graph(cursor):
     cursor.execute(f"SELECT author_start, code FROM {TABLE} WHERE longevity='NULL'")
 
@@ -52,8 +54,6 @@ def author_graph(cursor):
         if atribute[0] in author_list:
             len_lines[atribute[0]] += 1
 
-    print(len_lines)
-
     for item, value  in len_lines.items():
         # Fallo GitHub duda!
         if item != 'GitHub  ':
@@ -64,7 +64,57 @@ def author_graph(cursor):
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig('CurrentLines_graph.png')
-    plt.show()  
+
+# Churn rate graph
+def churn_graph(cursor):
+    cursor.execute(f"SELECT file_name, code FROM {TABLE} WHERE longevity='NULL'")
+
+    files = []
+    tuples = cursor.fetchall()
+
+    for atribute in tuples:
+        files.append(atribute[0])
+
+    conteo = Counter(files)
+    tuples_counter = tuple(conteo.items())
+
+    # Lista de archivos
+    files_list = [item[0] for item in tuples_counter]
+
+    len_del_lines = {value: 0 for value in files_list}
+
+    for atribute in tuples:
+        if atribute[0] in files_list:
+            len_del_lines[atribute[0]] += 1
+
+    cursor.execute(f"SELECT file_name, code FROM {TABLE}")
+
+    tuples = cursor.fetchall()
+
+    total_len_lines = {value: 0 for value in files_list}
+
+    for atribute in tuples:
+        if atribute[0] in files_list:
+            total_len_lines[atribute[0]] += 1
+
+    churn_rate = {value: 0 for value in files_list}
+
+    for atribute in tuples:
+        if atribute[0] in files_list:
+            churn_rate[atribute[0]] = len_del_lines[atribute[0]] / total_len_lines[atribute[0]] * 100
+
+    plt.figure(figsize=(11, 5))
+    for item, value  in churn_rate.items():
+        # Fallo GitHub duda!
+        if item != 'GitHub  ':
+            plt.stem(item, value, linefmt='green')
+    plt.xlabel('Archivos')
+    plt.ylabel('Tasa de Churn [%]')
+    plt.grid(axis='y', linestyle='--')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('ChurnRate_graph.png')
+ 
 
 if __name__ == "__main__":
     get_graphs()
